a717751a2f6c6c0c1fe5b470387ee997199f1be8
rajivranjanmars <67179338+rajivranjanmars@users.noreply.github.com>
2021-02-22 07:18:38 +0530

    Add files via upload

---
+++ getting started/p53.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+int sum = 0, value = 0;
+/*
+*If we give this program the input
+*3 4 5 6
+*then our output will be
+*Sum is: 18
+*/
+// read until end-of-file, calculating a running total of all values read
+while (std::cin >> value)
+sum += value; // equivalent to sum = sum + value
+std::cout << "Sum is: " << sum << std::endl;
+return 0;
+}
