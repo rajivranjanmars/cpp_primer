2d770038aec45e36acf240f2b0a561cea51c1f34
rajivranjanmars <67179338+rajivranjanmars@users.noreply.github.com>
2021-03-02 18:27:38 +0530

    Add files via upload

---
+++ variable and basic type/ex2_6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+int main(){
+
+    int month = 9, day = 7;
+   // int month = 09, day = 07;
+           // error comes as 09 is not valid in octal    
+ return 0;
+}
