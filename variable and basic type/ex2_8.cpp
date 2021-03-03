902ead4f7752377a70079d1a034f0b3d3119f1ed
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2021-03-03 07:59:44 +0530

    Add files via upload

---
+++ variable and basic type/ex2_8.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+    cout << "\62\115\12 " << endl;
+    cout << "\62 \11\115\12";
+    return 0;
+}
