1d18973392c98aee3087920e9c015309375a98ef
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2021-03-06 09:17:20 +0530

    Add files via upload

--- variable and basic type/ex2_10.cpp
+++ variable and basic type/ex2_10.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
 #include <cstdio>
+#include <iostream>
 #include <string>
 using namespace std;
- std::string global_str;
+std::string global_str;
 int global_int;
- void glo();
+void glo();
 int main()
 {
-int local_int;
-std::string local_str;
-cout << local_int << endl << local_str << endl; // int= garbage , string is empty
-glo();
+    int local_int;
+    std::string local_str;
+    cout << local_int << endl
+         << local_str << endl; // int= garbage , string is empty
+    glo();
 }
-void glo(){
-    cout << global_int << endl << global_str << endl; // int =0; string is empty
+void glo()
+{
+    cout << global_int << endl
+         << global_str << endl; // int =0; string is empty
 }
