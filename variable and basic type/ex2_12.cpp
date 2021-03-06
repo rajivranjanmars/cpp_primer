47756b3d98fc7162c920a5d362d1beb9575124ab
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2021-03-06 09:17:57 +0530

    Add files via upload

---
+++ variable and basic type/ex2_12.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+//   int double = 3.14;   invalid because double is reseved
+    int _;                /// allowed under a function
+//  int catch - 22;       // catch is reserved word
+//  int 1_or_2 = 1;       // name can't be start from number
+    double Double = 3.14; // works fine because of capital letter , cpp is case sensitive
+
+    return 0;
+}
