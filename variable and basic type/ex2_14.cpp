562ecf206d0575f6e55b095aad24211668f259f8
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2021-03-07 09:47:12 +0530

    Add files via upload

---
+++ variable and basic type/ex2_14.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int i = 100, sum = 0;
+    for (int i = 0; i != 10; ++i)
+        sum += i;
+    std::cout << i << " " << sum << std::endl;
+    return 0;
+}
