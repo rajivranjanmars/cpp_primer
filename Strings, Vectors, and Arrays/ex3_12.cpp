4e7bb64abbbde1d5e9ca6264b0a49157df174ace
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-05 08:55:42 +0530

    .

---
+++ Strings, Vectors, and Arrays/ex3_12.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main(){
+
+vector<vector<int>> ivec;
+ //vector<string> svec = ivec;   //illegal
+ vector<string> svec(10, "null");
+
+
+ return 0;
+}
